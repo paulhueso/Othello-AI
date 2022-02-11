4d4e5c6c00f3eb5f7022c4ec3a23b4f9f5626a5a
Mathias COUSSAT <mathias.coussat@free.fr>
2022-02-11 14:36:54 +0100

    add getAllSlotsAvailable

--- core.py
+++ core.py
@@ -1,4 +1,3 @@
-
 class Board:
     """Core game for Othello"""
     
@@ -21,18 +20,53 @@ class Board:
     def detectAround(self, x ,y ,colorCheck):
         directions = []
         for j in range(-1, 2):
-           for i in range(-1, 2):
-                newX = x + i
-                newY = y + j
-                if(newX < 0
-                otherColor = self.board[y + j][x + i]
-                if(otherColor != self.empty and colorCheck != otherColor):
-                    directions.append((i,j))
+            newY = y + j
+            if(newY >= 0 and newY <= 7):
+                for i in range(-1, 2):
+                    newX = x + i
+                    if(newX >= 0 and newX <= 7):
+                        otherColor = self.board[newY][newX]
+                        if(otherColor != self.empty and colorCheck != otherColor):
+                            directions.append((i,j))
         return directions
                
-    def isPlayable(self, x, y):
+    def isPlayable(self, x, y, colorCheck):
         if(x < 0 or x > 7 or y < 0 or y > 7):
             return False
-        
+        currentColor = self.board[y][x]
+        if(currentColor != self.empty):
+            return False
+        directions = self.detectAround(x,y,colorCheck)
+        for directionX, directionY in directions:
+            otherColor = self.board[y + directionY][x + directionX]
+            nbOfSlot = 2
+            isValid = True
+            keepContinue = True
+            while(keepContinue):
+                newX = x + directionX * nbOfSlot
+                newY = y + directionY * nbOfSlot
+                if(newY < 0 or newY > 7 or newX < 0 or newX > 7):
+                    keepContinue = False
+                    isValid = False
+                else:
+                    newColor = self.board[newY][newX]
+                    if(newColor == otherColor):
+                        nbOfSlot += 1
+                    elif (newColor == self.empty):
+                        keepContinue = False
+                        isValid = False
+                    else:
+                        keepContinue = False
+            if(isValid):
+                return True
+        return False
+
+    def getAllSlotsAvailable(self, colorCheck):
+        slots = []
+        for x in range(8):
+            for y in range(8):
+                if(self.isPlayable(x, y, colorCheck)):
+                   slots.append((x,y))
+        return slots
 
 othello = Board()
