0a85d6c65f1cb7b8210d8937b91062651652be88
Mathias COUSSAT <mathias.coussat@free.fr>
2022-02-11 15:23:25 +0100

    Add times and rework display

--- core.py
+++ core.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 class Board:
     """Core game for Othello"""
     
@@ -10,12 +13,48 @@ class Board:
         self.board[4][3] = self.player2
         self.board[3][4] = self.player2
         self.board[4][4] = self.player1
+        self.board[4][4] = self.player1
+
+    def random(self, propPlayer1, propPlayer2):
+        for x in range(8):
+            for y in range(8):
+                nb = random.random()
+                if(nb < propPlayer1):
+                    self.board[y][x] = self.player1
+                elif(nb < propPlayer2):
+                    self.board[y][x] = self.player2
+                else:
+                    self.board[x][y] = self.empty
 
     def display(self):
+        text = ""
         for row in self.board:
-            for text in row:
-                print(text, end=" | ")
-            print()
+            for slot in row:
+                if(slot == self.player1):
+                    text += "○|"
+                elif(slot == self.player2):
+                    text += "●|"
+                else:
+                    text += "_|"
+            text += "\n"
+        print(text)
+
+    def displayPossibilities(self, colorCheck):
+        slots = self.getAllSlotsAvailable(colorCheck)
+        text = ""
+        for j in range(8):
+            for i in range(8):
+                slot = self.board[j][i]
+                if((i,j) in slots):
+                    text += str(slots.index((i,j))) + "|"
+                elif(slot == self.player1):
+                    text += "○|"
+                elif(slot == self.player2):
+                    text += "●|"
+                else:
+                    text += "_|"
+            text += "\n"
+        print(text)
 
     def detectAround(self, x ,y ,colorCheck):
         directions = []
@@ -70,3 +109,13 @@ class Board:
         return slots
 
 othello = Board()
+
+# Calcul of time execution
+
+##othello.random(0.3,0.5)
+##start = time.time()
+##for i in range(1000):
+##    othello.getAllSlotsAvailable(2)
+##end = time.time()
+##othello.displayPossibilities(2)
+##print("--- %s seconds ---" % (end - start))
