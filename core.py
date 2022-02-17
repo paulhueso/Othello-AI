0f377e57cfab17f7ceb0fc8df809b302b1d3f5af
Mathias COUSSAT <mathias.coussat@free.fr>
2022-02-17 17:07:28 +0100

    Add reinforcement training

--- core.py
+++ core.py
@@ -1,7 +1,9 @@
+import board
+import training
+import ia
+import neuralNetwork
 import time
 import random
-import board
-import ia
 # TODO: créer une fonction qui récupère toutes les positions des jetons d'un joueur (peut être implémenter un tableau qui est modifié selon les positions des jetons)
 
 class Core:
@@ -11,14 +13,17 @@ class Core:
         self.turn = 0
         self.player = 1
         self.board = board.Board()
+        self.numberOfMoves = 0
+        self.generateStart()
+    
+    def generateStart(self):
         self.currentBoard = [[self.board.empty for i in range(8)] for j in range(8)]
         self.currentBoard[3][3] = self.board.player1
         self.currentBoard[4][3] = self.board.player2
         self.currentBoard[3][4] = self.board.player2
         self.currentBoard[4][4] = self.board.player1
-        self.numberOfMoves = 0
 
-    def inputInArray(self, array, question = "Please, choose.", error = "Votre entrée n'est pas valide."):
+    def inputInArray(self, array, question = "Please, choose.", error = "Your input isn't valid."):
         x = ""
         if(len(array) == 0):
             return x
@@ -39,13 +44,16 @@ class Core:
         human = ["h", "H", "human", "Human"]
         ia = ["i","I","ia","IA","AI","ai","a"]
         random = ["r", "R", "random", "Random", "rand", "Rand"]
-        p = self.inputInArray(human + ia + random, "Is the player" + str(numPlayer) + " Human, an AI or Random ?")
+        neural = ["n"]
+        p = self.inputInArray(human + ia + random + neural, "Is the player" + str(numPlayer) + " Human, an AI or Random ?")
         if(p in human):
             p = 0
         elif(p in ia):
             p = 1
-        else:
+        elif(p in random):
             p = 2
+        else:
+            p = 3
         return p
 
     def choosePlayers(self):
@@ -54,11 +62,19 @@ class Core:
             depth = input("\nAI depth: ")
             strategy = input("\nStrategy: \n1) Absolute\n2) Positional\n3) Mobility\n4) Mixte (Recommended)\nChoice: ")
             self.ia1 = ia.IA(self.board.player1, self.board.player2, int(depth), int(strategy))
+        elif(p1[1] == 3):
+            self.neural1 = neuralNetwork.NeuralNetwork()
+            fileName = input("Which AI would you like to load ?")
+            self.neural1.load(fileName)
         p2 = (self.board.player2, self.choosePlayer(2))
         if(p2[1] == 1):
             depth = input("AI depth: ")
             strategy = input("\nStrategy: \n1) Absolute\n2) Positional\n3) Mobility\n4) Mixte (Recommended)\nChoice: ")
             self.ia2 = ia.IA(self.board.player2, self.board.player1, int(depth), int(strategy))
+        elif(p2[1] == 3):
+            self.neural2 = neuralNetwork.NeuralNetwork()
+            fileName = input("Which AI would you like to load ?")
+            self.neural2.load(fileName)
         return [p1, p2]
 
     def doTurn(self, player, times):
@@ -96,6 +112,18 @@ class Core:
             self.board.playSlot(self.currentBoard, x, y, color)
             end = time.time()
             times[color - 1] += end - start
+        elif(nbType == 3): # neural network
+            if(color == self.board.player1):
+                start = time.time()
+                bestMove = self.neural1.chooseProposition(self.currentBoard, color)
+                end = time.time()
+                times[0] += end - start
+            else:
+                start = time.time()
+                bestMove = self.neural2.chooseProposition(self.currentBoard, color)
+                end = time.time()
+                times[1] += end - start
+            self.board.playProposition(self.currentBoard, bestMove, color)
         self.numberOfMoves += 1
         return True
 
@@ -122,6 +150,7 @@ class Core:
         print("Done !")
             
     def runOthello(self):
+        self.generateStart()
         players = self.choosePlayers()
         played1 = True
         played2 = True
@@ -144,17 +173,29 @@ class Core:
             self.saveScores((players[0][1], 0),(players[1][1], 1))
         print("The average time for player1 is : " + str(times[0] / turns) + " s !")
         print("The average time for player2 is : " + str(times[1] / turns) + " s !")
+    
+    def runMenu(self):
+        run = True
+        while(run):
+            fight = ["f", "F", "fight", "FIGHT", "1"]
+            train = ["t","T", "train", "TRAIN", "2"]
+            quit = ["q","Q", "quit", "QUIT", "3"]
+            print("""What do you want to do ?
+            1) FIGHT !
+            2) Train AI
+            3) Quit""")
+            choice = self.inputInArray(fight + train + quit)
+            if(choice in fight):
+                self.runOthello()
+            elif(choice in train):
+                tournamentSize = int(input("Choose the size of the tournament (recommended = 5) :\n"))
+                nbGenerations = int(input("Choose the number of generation (~ 1 min per generation for 5 in size of tournament) :\n"))
+                trainingAI = training.Training(tournamentSize)
+                bestFileName = input("Enter the name of the current best AI (press enter if there is not) :\n")
+                trainingAI.runTraining(nbGenerations, bestFileName)
+            else:
+                run = False
 
 
 core = Core()
-core.runOthello()
-
-# Calcul of time execution
-
-##othello.random(0.3,0.5)
-##start = time.time()
-##for i in range(1000):
-##    othello.getAllSlotsAvailable(2)
-##end = time.time()
-##othello.displayPossibilities(2)
-##print("--- %s seconds ---" % (end - start))
+core.runMenu()
---
+++ training.py
@@ -0,0 +1,110 @@
+import random
+import neuralNetwork
+import board
+import copy
+import os
+import time
+
+class Training:
+    def __init__(self, sizeTournament):
+        self.nbParticipants = 2**sizeTournament
+        self.nbGenerations = 0
+        self.currentGeneration = []
+        self.currentNbGeneration = 0
+        self.board = board.Board()
+        self.totalTime = 0
+        self.bestOfLastGeneration = None
+
+    def initNeuralNetworks(self, inputLength = 64, layersLength = [16, 16, 16], outputLength = 64):
+        self.currentGeneration = []
+        for i in range(self.nbParticipants):
+            self.currentGeneration.append(neuralNetwork.NeuralNetwork(inputLength, layersLength, outputLength))
+        for network in self.currentGeneration:
+            network.randomFullPopulate()
+    
+    def completeGeneration(self, best):
+        if(not(os.path.exists("generations"))):
+            os.makedirs("generations")
+        self.bestOfLastGeneration.save("generations\\" + str(self.currentNbGeneration) + ".txt")
+        self.bestOfLastGeneration = best
+        avgTimePerGame = self.totalTime / (self.currentNbGeneration * (self.nbParticipants - 1))
+        print("The average time per game is " + str(avgTimePerGame) + "s.")
+        estimatedTime = avgTimePerGame * (self.nbParticipants - 1) * (self.nbGenerations - self.currentNbGeneration)
+        print("The estimated time of arrival is : " + str(int(estimatedTime / 60)) + "m " + str(int(estimatedTime) % 60) +"s.")
+        print("End of generation n°" + str(self.currentNbGeneration) + " .")
+    
+    def regenerateGeneration(self):
+        self.currentGeneration = [self.bestOfLastGeneration]
+        currentMutationRatio = 0.05
+        for i in range(1, self.nbParticipants):
+            self.currentGeneration.append(copy.deepcopy(self.bestOfLastGeneration))
+            self.currentGeneration[i].mutate(currentMutationRatio)
+            currentMutationRatio += 0.05
+    
+    def doTournament(self):
+        nextRound = [i for i in range(self.nbParticipants)]
+        while(len(nextRound) > 1):
+            choices = [nextRound[i] for i in range(len(nextRound))]
+            currentRound = []
+            nextRound = []
+            while(len(choices) > 0):
+                adversary1 = random.choice(choices)
+                choices.remove(adversary1)
+                adversary2 = random.choice(choices)
+                choices.remove(adversary2)
+                currentRound.append((adversary1, adversary2))
+            for adversary1, adversary2 in currentRound:
+                currentBoard = [[self.board.empty for i in range(8)] for j in range(8)]
+                currentBoard[3][3] = self.board.player1
+                currentBoard[4][3] = self.board.player2
+                currentBoard[3][4] = self.board.player2
+                currentBoard[4][4] = self.board.player1
+                player1 = self.currentGeneration[adversary1]
+                player2 = self.currentGeneration[adversary2]
+                played1 = True
+                played2 = True
+                while(played1 or played2):
+                    bestMove = player1.chooseProposition(currentBoard, self.board.player1)
+                    if(bestMove != -1):
+                        self.board.playProposition(currentBoard, bestMove, self.board.player1)
+                        played1 = True
+                    else:
+                        played1 = False
+                    bestMove = player2.chooseProposition(currentBoard, self.board.player2)
+                    if(bestMove != -1):
+                        self.board.playProposition(currentBoard, bestMove, self.board.player2)
+                        played2 = True
+                    else:
+                        played2 = False
+                results = self.board.getScores(currentBoard)
+                if(results[0] > results[1]):
+                    nextRound.append(adversary1)
+                else:
+                    nextRound.append(adversary2)
+            print("End of the round with " + str(len(currentRound)*2) + " players.")
+        return nextRound[0]
+
+    
+    def runTraining(self, nbGenerations, fileBest = None):
+        avgTimePerGame = 0
+        self.currentNbGeneration = 0
+        self.nbGenerations = nbGenerations
+        self.totalTime = 0
+        print("Started the training of the AI with " + str(nbGenerations) + " generations, and " + str(self.nbParticipants) + " participants per tournament.")
+        if(not(fileBest == None)):
+            self.bestOfLastGeneration = neuralNetwork.NeuralNetwork()
+            self.bestOfLastGeneration.load(fileBest)
+        else:
+            self.bestOfLastGeneration = neuralNetwork.NeuralNetwork(64, [16,16,16], 64)
+            self.bestOfLastGeneration.randomFullPopulate()
+
+        while(self.currentNbGeneration < self.nbGenerations):
+            self.currentNbGeneration += 1
+            self.regenerateGeneration()
+            start = time.time()
+            bestOfGeneration = self.doTournament()
+            end = time.time()
+            self.totalTime += end - start
+            self.completeGeneration(self.currentGeneration[bestOfGeneration])
+        self.bestOfLastGeneration.save()
+        print("Ended the training of the AI.")
